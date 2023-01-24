719272ff664f3c2f793e1225533389bf726fa488
mycroft-stuff <jonasbrandon00@gmail.com>
2023-01-24 15:52:42 +0100

    wfefe

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.skills.audioservice import AudioService
 
 class RadioParadiseMain(MycroftSkill):
     def __init__(self):
@@ -8,8 +8,15 @@ class RadioParadiseMain(MycroftSkill):
     @intent_file_handler('main.paradise.radio.intent')
     def handle_main_paradise_radio(self, message):
         self.speak_dialog('main.paradise.radio')
+        
 
 
 def create_skill():
     return RadioParadiseMain()
 
+
+def initialize(self):
+    self.audio_service = AudioService(self.bus)
+    
+    
+self.audio_service.play(' http://stream.radioparadise.com/mp3-320')
