c53e02e4c731948a16630217e53ec8d778419a2b
mycroft-stuff <jonasbrandon00@gmail.com>
2023-01-24 15:59:10 +0100

    adfe

--- __init__.py
+++ __init__.py
@@ -4,12 +4,13 @@ from mycroft.skills.audioservice import AudioService
 class RadioParadiseMain(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.audio_service = AudioService(self.bus)
-        self.audio_service.play(' http://stream.radioparadise.com/mp3-320')
+
 
     @intent_file_handler('main.paradise.radio.intent')
     def handle_main_paradise_radio(self, message):
         self.speak_dialog('main.paradise.radio')
+        self.audio_service = AudioService(self.bus)
+        self.audio_service.play(' http://stream.radioparadise.com/mp3-320')
         
 
         
