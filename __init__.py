a1ea3581811adf7a2cb2dcb3f6132c1b4022e3aa
mycroft-stuff <jonasbrandon00@gmail.com>
2023-01-24 16:00:43 +0100

    fese

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ class RadioParadiseMain(MycroftSkill):
     def handle_main_paradise_radio(self, message):
         self.speak_dialog('main.paradise.radio')
         self.audio_service = AudioService(self.bus)
-        self.audio_service.play(' http://stream.radioparadise.com/mp3-320')
+        self.audio_service.play('http://stream.radioparadise.com/mp3-320')
         
 
         
